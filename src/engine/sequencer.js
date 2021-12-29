c7202b647fe88423c5c88022ce6e54a178c38126
Patric Feldmeier <patric.feldmeier@uni-passau.de>
2021-12-29 16:35:59 +0100

    Step through each thread exactly once

--- src/engine/sequencer.js
+++ src/engine/sequencer.js
@@ -81,14 +81,11 @@ class Sequencer {
         // Whether `stepThreads` has run through a full single tick.
         let ranFirstTick = false;
         const doneThreads = [];
-        // Conditions for continuing to stepping threads:
-        // 1. We must have threads in the list, and some must be active.
-        // 2. Time elapsed must be less than WORK_TIME.
-        // 3. Either turbo mode, or no redraw has been requested by a primitive.
-        while (this.runtime.threads.length > 0 &&
-               numActiveThreads > 0 &&
-               this.timer.timeElapsed() < WORK_TIME &&
-               (this.runtime.turboMode || !this.runtime.redrawRequested)) {
+        // The sequencer steps through each thread once if there is at least one active thread and no redraw
+        // has been requested.
+        while (!ranFirstTick && this.runtime.threads.length > 0 &&
+        numActiveThreads > 0 &&
+        (this.runtime.turboMode || !this.runtime.redrawRequested)) {
             if (this.runtime.profiler !== null) {
                 if (stepThreadsInnerProfilerId === -1) {
                     stepThreadsInnerProfilerId = this.runtime.profiler.idByName(stepThreadsInnerProfilerFrame);
